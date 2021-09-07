fa41b5830c84b327db1c43bbf8dc771ab7b6e7f6
Anderson Melo <andyh.melo88@gmail.com>
2021-09-07 20:58:32 -0300

    Initial commit

---
+++ exercicio_formatacao.py
@@ -0,0 +1,6 @@
+#1. Dadas as variáveis:
+Planeta = "Terra"
+Diametro = 12742
+#Use format() para printar a seguinte frase:
+#O diâmetro da terra é de 12742 kilômetros.
+print('O diâmetro da {} é de {} kilômetros. '.format(Planeta, Diametro))
